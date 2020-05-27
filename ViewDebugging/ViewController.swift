27bb6ecfd02a1cfc9a5eb9e8597e197826118a6a
Gabriel Sória <gabrielss.rock@me.com>
2020-05-26 21:01:29 -0300

    Initial Commit

---
+++ ViewDebugging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewDebugging
+//
+//  Created by Gabriel Soria Souza on 26/05/20.
+//  Copyright © 2020 Gabriel Sória Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
